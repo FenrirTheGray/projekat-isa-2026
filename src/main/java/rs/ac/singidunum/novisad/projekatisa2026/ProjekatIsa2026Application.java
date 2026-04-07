e5c965c0c46303de80f689f85805076547d0b8d0
Aleksandar Colovic <aleksandar.c.dev@gmail.com>
2026-04-07 19:43:48 +0200

    Add initial project setup

---
+++ src/main/java/rs/ac/singidunum/novisad/projekatisa2026/ProjekatIsa2026Application.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.novisad.projekatisa2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatIsa2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjekatIsa2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/rs/ac/singidunum/novisad/projekatisa2026/ProjekatIsa2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.novisad.projekatisa2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekatIsa2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
